b2f64fd6840fee1f6c53dffc5a63cc2a9e0e62e8
shreyaras <shreya31.sr@gmail.com>
2021-10-09 18:31:40 +0530

    Update UrlShortnerController.java

--- url-shortner/src/main/java/com/cybrilla/controller/UrlShortnerController.java
+++ url-shortner/src/main/java/com/cybrilla/controller/UrlShortnerController.java
@@ -1,6 +1,8 @@
 package com.cybrilla.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +16,14 @@ public class UrlShortnerController {
 	@Autowired
 	private UrlShortnerService service;
 	
-	@PostMapping("/url")
+	@PostMapping("/shortenUrl")
 	public String shortenUrl(@RequestBody UrlPOJO url) {
-	 return service.saveUrl(url);
+	 return service.shortenUrl(url);
+	}
+	
+	@PostMapping("/getOriginalUrl")
+	public String getOriginalUrl(@RequestBody UrlPOJO url) {
+		return service.getOriginalUrl(url);
 	}
 
 }
